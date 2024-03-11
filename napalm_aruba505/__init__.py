0fba324e44aa56fa3ea4f12f05f17528a4bda99f
Karpe, Sonja <skarpe@bethel.jw.org>
2024-03-11 11:37:19 +0000

    Version 0.0.167

--- napalm_aruba505/__init__.py
+++ napalm_aruba505/__init__.py
@@ -1,6 +1,7 @@
 """load napalm_aruba505.aruba505 class."""
 # load ArubaDriver class.
 
-# from napalm_aruba505.aruba505 import Aruba505Driver
 from napalm_aruba505.aruba505 import Aruba505Driver
 __all__ = ('Aruba505Driver',)
+
+__version__ = "0.0.167"
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "napalm_aruba505",
-    version = "0.0.166",
+    version = "0.0.167",
     author = "David Johnnes",
     author_email = "david.johnnes@gmail.com",
     description = ("Napalm Aruba driver for ArubaOS Wi-Fi devices '505' "),
